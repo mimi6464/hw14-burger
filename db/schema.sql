817662685fa6fa69b761b933a7750defa551c8e9
tsionasmamaw <tayimer@yahoo.com>
2016-10-06 10:09:19 -0700

    almost

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+-- // in the db folder, create a file named schema.sql. Write SQL queries this file that do the following:
+-- // Create the burgers_db.
+-- // Switch to or use the burgers_db.
+-- // Create a burgers table with these fields:
+-- // id: an auto incrementing int that serves as the primary key.
+-- // burger_name: a string.
+-- // devoured: a boolean.
+-- // date: a TIMESTAMP.
+
+### Schema
+-- CREATE DATABASE burgers_db;
+-- USE burgers_db;
+
+-- CREATE TABLE burgers
+-- (
+--  id int NOT NULL AUTO_INCREMENT,
+--  burger_name varchar(255) NOT NULL,
+--  devoured boolean DEFAULT 0,
+--  date TIMESTAMP DEFAULT now() ON UPDATE now(),
+--  PRIMARY KEY (id)
+-- );
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers(
+id int AUTO_INCREMENT,
+PRIMARY KEY (id),
+burger_name varchar(50) NOT NULL,
+devoured BOOLEAN default false,
+date TIMESTAMP 
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+
+
+INSERT INTO burgers (burger_name) VALUES ('Bacon Tendergrill');
+
+INSERT INTO burgers (burger_name) VALUES ('Double Double');
+INSERT INTO burgers (burger_name) VALUES ('Big Macc Meat');
+
+INSERT INTO burgers (burger_name) VALUES ('Killer Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Big Mac with Extra Cheese');
+
